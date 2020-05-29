75bfee400b118a5c548d7ab9930edca7e48a4c5e
Sudip Sinha <SudipSinha.Bappa@Gmail.com>
2020-05-29 13:04:13 -0600

    Deployed bbda02e with MkDocs version: 1.1

---
+++ javascripts/macros4mathjax.js
@@ -0,0 +1,22 @@
+window.MathJax = {
+	loader: {load: ["[tex]/cancel", "input/tex", "output/chtml"]},
+	// http://docs.mathjax.org/en/latest/input/tex/extensions.html
+	// http://docs.mathjax.org/en/latest/output/index.html
+	tex: {
+		packages: {"[+]": ["cancel"]},
+		tags: "ams",    // http://docs.mathjax.org/en/latest/input/tex/eqnumbers.html
+		macros: {    // http://docs.mathjax.org/en/latest/input/tex/macros.html
+			// Brackets
+			brnd: [" \\left( #1 \\right) ", 1],
+			bcrl: [" \\left\\{ #1 \\right\\} ", 1],
+			bsqr: [" \\left[ #1 \\right] ", 1],
+			// Commonly used delimiters
+			abs: [" \\left| #1 \\right| ", 1],
+			norm: [" \\left\\lVert #1 \\right\\rVert ", 1],
+			inn: [" \\left\\langle #1 \\right\\rangle ", 1],
+			pair: [" \\left⟨ #1 \\right⟩ ", 1],
+			floor: [" \\left⌊ #1 \\right⌋ ", 1],
+			ceil: [" \\left⌈ #1 \\right⌉ ", 1],
+		}
+	}
+};
